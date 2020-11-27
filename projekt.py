eb85316c2f3789c8e92871d67fdf588f245f3c3a
Andrei Redi <andrei.redi@ut.ee>
2020-11-27 19:18:09 +0200

    Lisasin Click CLI funktsioone

--- projekt.py
+++ projekt.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python3
+
 import pickle
 import json
 import requests
 from os import path, environ
 from datetime import date, time, datetime 
+
+import click
 
 URL = 'https://docs.google.com/forms/d/e/1FAIpQLScZS4UrEoWnGTwJRk1tlnFBYC_5NseUuGw_M0b9_dEJVlcD4Q/formResponse'
 forms_seaded= {   
@@ -15,12 +19,13 @@ forms_seaded= {
     'kellaaeg':'entry.1339978028'
 }
 
-def kuupäev():   
+
+def päev():   
     aeg = datetime.now().date()
     return aeg.strftime('%d %m %Y').split()  # salvestan kuupäeva listi formaadis [päev, kuu, aasta]
 
 
-def kellaaeg():     # võtab praeguse kellaaja ja tagastab selle õiges vahemikus formi jaoks
+def aeg():     # võtab praeguse kellaaja ja tagastab selle õiges vahemikus formi jaoks
     aeg = datetime.now().time()
     
     if aeg < time(10,00):
@@ -42,9 +47,12 @@ def kellaaeg_sõne(aeg1, aeg2):
     
 def esimene_käivitus():       
     ### esmakordsel käivitamisel küsitakse nimi ja matrikli/number või isikukood
+    #    
+    click.get_current_context()
 
-    nimi = input('Sisesta ees-ja perekonnanimi: ')
-    matrikel = input('Sisesta matrikli number või isikukood: ')
+    nimi = click.prompt('Sisesta ees-ja perekonnanimi', type=str)
+    matrikel = click.prompt('Sisesta matrikli number või isikukood', type=str)
+
 
     ### salvestab andmed objekti ja siis faili
 
@@ -67,50 +75,87 @@ def andmete_saatmine(andmed, seaded):
 
     return tulemus  
 
+
 ##################################################
 ####  PÕHIPROGRAMM  ##############################
 ##################################################
-
-print('\nTegemist on skriptiga, mis sisestab andmeid registreerimislehele')
-print('Kuupäeva ja kellaaja tuletab programm ajast, mil programm käivitati.')
-
-
-if not (path.exists('save.p')):     # Kui käivitakse esimest korda, küsitakse nime ja matrikli numbrit
-    esimene_käivitus()
-
-andmed_failist = {}
-
-with open('save.p', 'rb') as failist:
-    andmed_failist = pickle.load(failist)
-#print(andmed_failist)
-
-kuupäev = kuupäev()
-kellaaeg = kellaaeg()
-
-print(f'\nNimi: {andmed_failist["nimi"]}  matrikli nr/isikukood: {andmed_failist["matrikel"]} kellaaeg: {kellaaeg}  kuupäev: {kuupäev[0]}-{kuupäev[1]}-{kuupäev[2]}\n')
-
-ruum = input('Sisesta ruumi number:  ')   
-
-andmed_failist['ruum']= ruum
-andmed_failist['aasta'] = kuupäev[2]
-andmed_failist['kuu'] = kuupäev[1]
-andmed_failist['päev'] = kuupäev[0]
-andmed_failist['kellaaeg'] = kellaaeg
-
-with open('save.p', 'wb') as faili:    #salvestan kogu info save.p faili
-    pickle.dump(andmed_failist, faili)
+click.help_option()
+@click.command()
 
 
-andmed = pickle.load(open('save.p', 'rb'))
+@click.argument('ruum', type=str, required=False)
+@click.option(
+    '-c', '--config', default=False, is_flag=True,
+    help='Kui soovid andmeid muuta, siis lisa see käsule juurde käivitamisel '
+    )
+
+def main(ruum, config):
+    '''
+    See Püütoni skript, aitab lihtsamini sisestada andmeid Delta registreerimislehele.\n
+    Kuupäeva ja kellaaja tuletab programm ajast, mil programm käivitati.
+
+    Skript salvestab nime ja matrikli koodi. Skripti käivitamisel on vaja lisada ainult ruuminumber. 
+     
+    '''
+    print('\nTegemist on skriptiga, mis sisestab andmeid registreerimislehele')
+    print('Kuupäeva ja kellaaja tuletab programm ajast, mil programm käivitati.')
+    print(config)
+
+    if not (path.exists('save.p')) or config:     # Kui käivitakse esimest korda, küsitakse nime ja matrikli numbrit
+        esimene_käivitus()
+    
+
+    with open('save.p', 'rb') as failist:
+        andmed_failist = pickle.load(failist)
+
+
+    kuupäev = päev()
+    kellaaeg = aeg()
+
+    
+    # kui ruumi koos skripti käivitamisega, ei sisestaud, küsitakse uuesti
+    if not ruum:
+        ruum = click.prompt('Sisesta ruumi number', type=str)
+
+    
+       
+    click.confirm(f'''
+
+Nimi: {andmed_failist["nimi"]}
+matrikli nr/isikukood: {andmed_failist["matrikel"]}
+kellaaeg: {kellaaeg}  
+kuupäev: {kuupäev[0]}-{kuupäev[1]}-{kuupäev[2]}
+ruum: {ruum}
+
+
+Kas saadan andmed ära?
+
+    ''', abort=True)
+
+    andmed_failist['ruum']= ruum
+    andmed_failist['aasta'] = kuupäev[2]
+    andmed_failist['kuu'] = kuupäev[1]
+    andmed_failist['päev'] = kuupäev[0]
+    andmed_failist['kellaaeg'] = kellaaeg
+
+    with open('save.p', 'wb') as faili:    #salvestan kogu info save.p faili
+        pickle.dump(andmed_failist, faili)
+
+
+    andmed = pickle.load(open('save.p', 'rb'))
 
 #print('@@', andmete_saatmine(andmed, forms_seaded))
   
-vastus = andmete_saatmine(andmed, forms_seaded)
+    vastus = andmete_saatmine(andmed, forms_seaded)
 
-if vastus.status_code == 200:
-     print("\nAndmed edukalt saadetud. ")
-else:
-    print("Andmete saatmisel tuli viga, proovige uuesti.")
+    if vastus.status_code == 200:
+        print("\nAndmed edukalt saadetud. ")
+    else:
+        print("Andmete saatmisel tuli viga, proovige uuesti.")
+
+
+if __name__ == "__main__":
+    main()
 
 
 
